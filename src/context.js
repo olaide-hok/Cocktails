ee5249125fa8e0a2a6afe2e7821eefe12b99fb8d
olaide-hok <olaidekareem43@yahoo.com>
2022-09-12 10:49:15 +0100

    Display list of Cocktails

--- src/context.js
+++ src/context.js
@@ -5,7 +5,7 @@ const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s='
 const AppContext = React.createContext()
 
 const AppProvider = ({ children }) => {
-  const [loading, setLoading] = useState(true)
+  const [loading, setLoading] = useState(false)
   const [searchTerm, setSearchTerm] = useState('a')
   const [cocktails, setCocktails] = useState([])
 
@@ -44,6 +44,9 @@ const AppProvider = ({ children }) => {
     }
   }
 
+  useEffect(() => {
+    fetchDrinks()
+  }, [searchTerm])
 
 
   return <AppContext.Provider value={{
